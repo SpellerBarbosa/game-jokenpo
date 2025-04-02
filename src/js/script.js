663801e2f00f4226cb93fab73819880996efb212
Fernando Barbosa <133154923+SpellerBarbosa@users.noreply.github.com>
2025-04-02 07:27:51 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+let idade = document.querySelector('#idade');
+let verificacao = document.querySelector('.verificacao');
+let regras = document.querySelector('.regras');
+let game = document.getElementsByClassName('game');
+let resposta = document.querySelectorAll(".resposta")
+
+function verificar (){
+
+
+}
